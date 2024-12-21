8f850b2fd9126c36f4fa9831d93a23a5741d532a
yash <yashsaundalkar0@gmail.com>
2024-12-21 20:29:43 +0530

    dsdf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App"; // Import the main App component
+import App from "./App";
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter basename="/PollFusion-/">
-    <App />
-  </BrowserRouter>,
+  <BrowserRouter basename="/PollFusion-">
+  <App />
+</BrowserRouter>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
